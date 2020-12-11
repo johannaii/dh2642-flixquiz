32c3c2863618417cd79e9426ed15a6523c17fdb9
Johanna Iivanainen <jii@kth.se>
2020-12-11 20:39:03 +0100

    fix/added styling and removed the correct quiz answer view

--- src/presenter/game.js
+++ src/presenter/game.js
@@ -1,170 +1,164 @@
-import React, {useEffect, useState, useContext} from "react";
+import React, { useEffect, useState, useContext } from "react";
 import GameView from "../view/gameView";
 import QuizCorrectAnswerView from "../view/quizCorrectAnswerView";
 import GameEndView from "../view/gameEndView";
 import axios from "axios";
-import {GameProvider} from "../context/activeGameContext";
-import {MovieStateContext} from "../context/activeMovieContext";
-import {UserStateContext} from "../context/activeUserContext";
+import { GameProvider } from "../context/activeGameContext";
+import { MovieStateContext } from "../context/activeMovieContext";
+import { UserStateContext } from "../context/activeUserContext";
+import { useHistory } from "react-router-dom";
 
 // Declares all functions used in the view
 // Listeners
 // Props...
 // Variables
-function Game({token, database}) {
-    // setStates
-    //const { currentQuestion, setCurrentQuestion } = useContext(GameProvider);
-    const [answered, setAnswered] = useState(0); // Show answer or not
-    const [finishedQuiz, setFinishedQuiz] = useState(0); // Finished quiz or not
-    const [currentQuestion, setCurrentQuestion] = useState(0); // Shows question #currentQuestion
-    //const [viewQuestion, setViewQuestion] = useState(0);
+function Game({ token, database }) {
+  // setStates
+  //const { currentQuestion, setCurrentQuestion } = useContext(GameProvider);
+  const [answered, setAnswered] = useState(0); // Show answer or not
+  const [finishedQuiz, setFinishedQuiz] = useState(0); // Finished quiz or not
+  const [currentQuestion, setCurrentQuestion] = useState(0); // Shows question #currentQuestion
+  //const [viewQuestion, setViewQuestion] = useState(0);
 
-    // for currentQuestion
-    const [correctAlternative, setCorrectAlternative] = useState("");
-    const [otherAlternatives, setOtherAlternatives] = useState(["", "", ""]);
+  // for currentQuestion
+  const [correctAlternative, setCorrectAlternative] = useState("");
+  const [otherAlternatives, setOtherAlternatives] = useState(["", "", ""]);
+  const { movie } = useContext(MovieStateContext);
+  const history = useHistory();
+  //
+  // Collecting first quiz from firebase
+  //
+  const [loading, setLoading] = useState(true);
 
-    const {movie} = useContext(MovieStateContext);
+  // Make sure to find quiz with title of chosen movie now just picks first quiz
+  useEffect(() => {
+    if (movie) {
+      setLoading(false);
+      // Setup first question
+      const questionInfo = movie?.questions[currentQuestion + 1];
 
-    //
-    // Collecting first quiz from firebase
-    //
-    const [loading, setLoading] = useState(true);
+      const correctTrackId = questionInfo.spotifyid;
+      const trackName = searchForSong(correctTrackId);
 
-    // Make sure to find quiz with title of chosen movie now just picks first quiz
-    useEffect(() => {
+      const albumId = movie?.spotifyid;
+      searchForOtherAlternatives(albumId, trackName);
 
-            setLoading(false);
-            // Setup first question
-            const questionInfo = movie.questions[currentQuestion + 1];
+      //console.log(movie);
+    } else {
+      history.push("/movies");
+    }
+  }, []);
 
-            const correctTrackId = questionInfo.spotifyid;
-            const trackName = searchForSong(correctTrackId);
+  //console.log(quiz);
+  //
+  // Collected quiz from firebase
+  //
 
-            const albumId = movie.spotifyid;
-            searchForOtherAlternatives(albumId, trackName);
+  const setAnswers = (questionInfo, trackId, albumId) => {
+    if (questionInfo.question === "What's the song title?") {
+      const trackName = searchForSong(trackId);
+      searchForOtherAlternatives(albumId, trackName);
+    } else {
+      setCorrectAlternative(() => questionInfo.answer);
+      setOtherAlternatives(() => [questionInfo.options, "alt2", "alt3"]);
+    }
+  };
 
-    }, []);
+  // Searches for song and saves name in correctAlternative state
+  const searchForSong = (trackId) => {
+    axios(` https://api.spotify.com/v1/tracks/${trackId}`, {
+      method: "GET",
+      headers: {
+        Authorization: "Bearer " + token,
+      },
+    }).then((tracksResponse) => {
+      if (tracksResponse.status === 200) {
+        const trackInfo = tracksResponse.data;
+        setCorrectAlternative(trackInfo.name);
+        return trackInfo.name;
+      }
+      return "";
+    });
+  };
+  // End of searchForSong
 
-    //console.log(quiz);
-    //
-    // Collected quiz from firebase
-    //
+  // Searches for an album and retrieves it's tracks
+  const searchForOtherAlternatives = (albumId, trackName) => {
+    axios(` https://api.spotify.com/v1/albums/${albumId}/tracks`, {
+      method: "GET",
+      headers: {
+        Authorization: "Bearer " + token,
+      },
+    }).then((tracksResponse) => {
+      //console.log(tracksResponse);
+      if (tracksResponse.status === 200) {
+        const almbumInfo = tracksResponse.data.items;
 
-    const setAnswers = (questionInfo, trackId, albumId) => {
-        if (questionInfo.question === "What's the song title?") {
-            const trackName = searchForSong(trackId);
-            searchForOtherAlternatives(albumId, trackName);
-        } else {
-            setCorrectAlternative(() => questionInfo.answer);
-            setOtherAlternatives(() => [questionInfo.options, "alt2", "alt3"]);
-        }
-    };
-
-    // Searches for song and saves name in correctAlternative state
-    const searchForSong = (trackId) => {
-        axios(`	https://api.spotify.com/v1/tracks/${trackId}`, {
-            method: "GET",
-            headers: {
-                Authorization: "Bearer " + token,
-            },
-        }).then((tracksResponse) => {
-            if (tracksResponse.status === 200) {
-                const trackInfo = tracksResponse.data;
-                setCorrectAlternative(trackInfo.name);
-                return trackInfo.name;
-            }
-            return "";
+        // Find three wrongAlternatives
+        let count = 0;
+        let tracks = ["", "", ""];
+        almbumInfo.forEach((track) => {
+          if (count < 3 && track.name !== trackName) {
+            tracks[count] = track.name;
+            count++;
+            //console.log(track.name);
+          }
         });
-    };
-    // End of searchForSong
+        setOtherAlternatives(tracks);
+      }
+    });
+  };
+  // End of searchForOtherAlternatives
 
-    // Searches for an album and retrieves it's tracks
-    const searchForOtherAlternatives = (albumId, trackName) => {
-        axios(`	https://api.spotify.com/v1/albums/${albumId}/tracks`, {
-            method: "GET",
-            headers: {
-                Authorization: "Bearer " + token,
-            },
-        }).then((tracksResponse) => {
-            //console.log(tracksResponse);
-            if (tracksResponse.status === 200) {
-                const almbumInfo = tracksResponse.data.items;
+  // Loads next question/ finishes quiz
+  const nextQuestion = (answer) => {
+    console.log(answer);
+    /* if (!answered) {
+      setAnswered(1);
+      // Display correct answer in green, and if wrong chosen that one in red
+      // check if correct answer then give points
+      // calculate points and add to totalPointsSoFar
+    } */
+    if (currentQuestion < movie?.questions.length - 2) {
+      const nextQuestionNum = currentQuestion + 1;
+      setCurrentQuestion(nextQuestionNum);
+      //setAnswered(0);
+      const questionInfo = movie?.questions[currentQuestion + 1];
 
-                // Find three wrongAlternatives
-                let count = 0;
-                let tracks = ["", "", ""];
-                almbumInfo.forEach((track) => {
-                    if (count < 3 && track.name !== trackName) {
-                        tracks[count] = track.name;
-                        count++;
-                        console.log(track.name)
-                    }
-                });
-                setOtherAlternatives(tracks);
-            }
-        });
-    };
-    // End of searchForOtherAlternatives
+      const correctTrackId = questionInfo.spotifyid;
+      const albumId = movie?.spotifyid;
 
-    // Loads next question/ finishes quiz
-    const nextQuestion = () => {
-        if (!answered) {
-            setAnswered(1);
-            // Display correct answer in green, and if wrong chosen that one in red
-            // check if correct answer then give points
-            // calculate points and add to totalPointsSoFar
-        } else if (currentQuestion < movie.questions.length - 2) {
-            const nextQuestionNum = currentQuestion + 1;
-            setCurrentQuestion(nextQuestionNum);
-            setAnswered(0);
-            const questionInfo = movie.questions[currentQuestion + 1];
+      setAnswers(questionInfo, correctTrackId, albumId);
+    } else {
+      //console.log("Finished quiz");
+      setFinishedQuiz(1);
+      // Do end of quiz stuff
+      // save points to firebase and show score page
+    }
+  };
+  // End of nextQuestion
 
-            const correctTrackId = questionInfo.spotifyid;
-            const albumId = movie.spotifyid;
-
-            setAnswers(questionInfo, correctTrackId, albumId);
-        } else {
-            console.log("Finished quiz");
-            setFinishedQuiz(1);
-            // Do end of quiz stuff
-            // save points to firebase and show score page
-        }
-    };
-    // End of nextQuestion
-
-    // Displays one of three views, quiz, quiz but showing answers, endGameView
-    if (!loading) {
-        if (!finishedQuiz) {
-            const questionInfo = movie.questions[currentQuestion + 1];
-            const question = questionInfo.question;
-            const correctTrackId = questionInfo.spotifyid;
-
-            if (!answered) {
-                return (
-                    <GameView
-                        currentQuestionNum={currentQuestion}
-                        nextQuestion={nextQuestion}
-                        correctTrackId={correctTrackId}
-                        correctAlternative={correctAlternative}
-                        wrongAlternatives={otherAlternatives}
-                        question={question}
-                    />
-                );
-            } else {
-                return (
-                    <QuizCorrectAnswerView
-                        currentQuestionNum={currentQuestion}
-                        nextQuestion={nextQuestion}
-                        correctAlternative={correctAlternative}
-                        wrongAlternatives={otherAlternatives}
-                        question={question}
-                    />
-                );
-            }
-        } else {
-            return <GameEndView score={"1000"}/>;
-        }
-    } else return <div>Is loading</div>;
+  // Displays one of three views, quiz, quiz but showing answers, endGameView
+  if (!loading) {
+    if (!finishedQuiz) {
+      const questionInfo = movie?.questions[currentQuestion + 1];
+      const question = questionInfo.question;
+      const correctTrackId = questionInfo.spotifyid;
+      return (
+        <GameView
+          currentQuestionNum={currentQuestion}
+          nextQuestion={nextQuestion}
+          correctTrackId={correctTrackId}
+          correctAlternative={correctAlternative}
+          wrongAlternatives={otherAlternatives}
+          question={question}
+        />
+      );
+    } else {
+      return <GameEndView score={"1000"} />;
+    }
+  } else return <div>Is loading</div>;
 }
 
 export default Game;
+
--- src/view/gameView.jsx
+++ src/view/gameView.jsx
@@ -1,58 +1,65 @@
 import React from "react";
+import "../styling/gameview.css";
+import { Button } from "semantic-ui-react";
 
 const GameView = ({
-    currentQuestionNum,
-    nextQuestion,
-    question,
-    correctTrackId,
-    correctAlternative,
-    wrongAlternatives
+  currentQuestionNum,
+  nextQuestion,
+  question,
+  correctTrackId,
+  correctAlternative,
+  wrongAlternatives,
 }) => {
-    const answerAlternatives = [correctAlternative, wrongAlternatives[0], wrongAlternatives[1], wrongAlternatives[2]];
-    //console.log("In game view:");
-    //console.log(answerAlternatives);
-    // Circle of Life id = "0juMU08O9byWiRBtKM1j5E";
-    // Circle of lifes album id ="7e8y48Z2fkJNGBOKSECCeS"
+  const answerAlternatives = [
+    correctAlternative,
+    wrongAlternatives[0],
+    wrongAlternatives[1],
+    wrongAlternatives[2],
+  ];
+  //console.log("In game view:");
+  //console.log(answerAlternatives);
+  // Circle of Life id = "0juMU08O9byWiRBtKM1j5E";
+  // Circle of lifes album id ="7e8y48Z2fkJNGBOKSECCeS"
 
-    // Life id = "6hdEgJdm3FFLvssWuoXKdV"
-    // const testId = "0juMU08O9byWiRBtKM1j5E";
+  // Life id = "6hdEgJdm3FFLvssWuoXKdV"
+  // const testId = "0juMU08O9byWiRBtKM1j5E";
 
-    // You can find an id of a spotify song through spotify by copying song URI
-    // You do this by rightclicking on a song, press share and copying the URI
-    // You get for example spotify:track:22KvbVZLVA9aY7zsRWK0fw
-    // The id is then 22KvbVZLVA9aY7zsRWK0fw
-    // To play the demo of the song just get the iframe and set id
+  // You can find an id of a spotify song through spotify by copying song URI
+  // You do this by rightclicking on a song, press share and copying the URI
+  // You get for example spotify:track:22KvbVZLVA9aY7zsRWK0fw
+  // The id is then 22KvbVZLVA9aY7zsRWK0fw
+  // To play the demo of the song just get the iframe and set id
 
-    return (
-        <div>
-            <h1>{"Question #" + (currentQuestionNum + 1)}</h1>
-            <div>{question}</div>
-            <iframe
-                src={
-                    "https://open.spotify.com/embed/track/" +
-                    correctTrackId /* correctAlternative.id */
-                }
-                width="80"
-                height="80"
-                frameBorder="0"
-                allowtransparency="true"
-                allow="encrypted-media"
-            ></iframe>
-            <div>
-                {/*random order on these 4*/}
-                {/* Randomly pick three others from album */}
-            <button>{answerAlternatives[0]}</button>
-            <button>{answerAlternatives[1]}</button>
-            <button>{answerAlternatives[2]}</button>
-            <button>{answerAlternatives[3]}</button>
-            </div>
-            <button onClick={nextQuestion}>Submit/ Show answer</button>
-            {/*This shows after nextQuestion button is pressed */}
-        </div>
-    );
+  return (
+    <div className="gameWrapper">
+      <span className="gameHeader">
+        {"Question " + (currentQuestionNum + 1)}
+      </span>
+      <span className="gameContent">{question}</span>
+      <iframe
+        src={
+          "https://open.spotify.com/embed/track/" +
+          correctTrackId /* correctAlternative.id */
+        }
+        width="80"
+        height="80"
+        frameBorder="0"
+        allowtransparency="true"
+        allow="encrypted-media"
+      ></iframe>
+      {/*random order on these 4*/}
+      {/* Randomly pick three others from album */}
+      {answerAlternatives?.map((answer, key) => (
+        <Button key={key} onClick={() => nextQuestion(answer)} className="gameButton">
+          {answer}
+        </Button>
+      ))}
+    </div>
+  );
 };
 
 export default GameView;
 
 // We can also have name of song then 4 to listen to
 // Or just random ass questions
+
