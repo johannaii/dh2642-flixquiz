fb810e3b869f5d408f6360804df4084f4629c4de
Johanna Iivanainen <jii@kth.se>
2020-12-13 15:26:24 +0100

    fix/highscore push back to highscores if no movie

--- src/presenter/highscore.js
+++ src/presenter/highscore.js
@@ -1,22 +1,28 @@
 import React, { useEffect, useState, useContext } from "react";
 import HighscoreView from "../view/highscoreView";
 import { MovieStateContext } from "../context/activeMovieContext";
+import { useHistory } from "react-router-dom";
 
 const Highscore = ({ database }) => {
   const [highscore, setHighscore] = useState([]);
   const { movie, setMovie } = useContext(MovieStateContext);
+  const history = useHistory();
 
   useEffect(() => {
-    const highscoreList = [];
-    database
-      ?.ref("highscore")
-      .child(movie.id)
-      .on("value", (snapshot) => {
-        snapshot.forEach((snap) => {
-          highscoreList.push(snap.val());
+    if (movie) {
+      const highscoreList = [];
+      database
+        ?.ref("highscore")
+        .child(movie.id)
+        .on("value", (snapshot) => {
+          snapshot.forEach((snap) => {
+            highscoreList.push(snap.val());
+          });
+          setHighscore(highscoreList);
         });
-        setHighscore(highscoreList);
-      });
+    } else {
+      history.push("/highscores")
+    }
   }, []);
 
   return <HighscoreView movie={movie} highscore={highscore} />;
