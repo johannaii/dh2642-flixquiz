73a0fabcecd98db3174d2130f4e198e970d62850
Johanna Iivanainen <jii@kth.se>
2020-12-13 17:35:03 +0100

    fix/fix button and url

--- src/presenter/game.js
+++ src/presenter/game.js
@@ -1,41 +1,45 @@
 import React, { useEffect, useState, useContext } from "react";
 import GameView from "../view/gameView";
 import GameEndView from "../view/gameEndView";
-import {MovieStateContext} from "../context/activeMovieContext";
-import {UserStateContext} from "../context/activeUserContext";
-import {useHistory} from 'react-router-dom';
+import { MovieStateContext } from "../context/activeMovieContext";
+import { UserStateContext } from "../context/activeUserContext";
+import { useHistory } from "react-router-dom";
 import LoadingView from "../view/loadingView";
 
-function Game({trackData, trackTitles, database}) {
+function Game({ trackData, trackTitles, database }) {
+  const { movie } = useContext(MovieStateContext);
+  const [currentQuestion, setCurrentQuestion] = useState(null);
+  const [currentQuestionNr, setCurrentQuestionNr] = useState(1);
+  const [loading, setLoading] = useState(true);
+  const [score, setScore] = useState(0);
+  const [finished, setFinished] = useState(false);
+  const { user } = useContext(UserStateContext);
+  const history = useHistory();
 
-    const {movie} = useContext(MovieStateContext);
-    const [currentQuestion, setCurrentQuestion] = useState(null);
-    const [currentQuestionNr, setCurrentQuestionNr] = useState(1);
-    const [loading, setLoading] = useState(true);
-    const [score, setScore] = useState(0);
-    const [finished, setFinished] = useState(false);
-    const {user} = useContext(UserStateContext);
-    const history = useHistory();
+  useEffect(() => {
+    if (movie) {
+      const answer = movie.questions[currentQuestionNr].answer
+        ? movie.questions[currentQuestionNr].answer
+        : trackData[currentQuestionNr - 1].name.split("-")[0];
 
-    useEffect(() => {
-        if (movie) {
-            const answer = movie.questions[currentQuestionNr].answer
-                ? movie.questions[currentQuestionNr].answer
-                : trackData[currentQuestionNr - 1].name.split('-')[0];
-
-            setCurrentQuestion({
-                'number': currentQuestionNr,
-                'spotifyid': movie.questions[currentQuestionNr].spotifyid,
-                'question': movie.questions[currentQuestionNr].question,
-                'answer': answer,
-                'options': movie.questions[currentQuestionNr].options ? shuffleList(movie.questions[currentQuestionNr].options.split(',').concat(answer)) : generateRandomOptions(answer),
-            })
-            setLoading(false)
-        } else {
-            history.push("/movies");
-        }
-    }, [currentQuestionNr]);
-
+      setCurrentQuestion({
+        number: currentQuestionNr,
+        spotifyid: movie.questions[currentQuestionNr].spotifyid,
+        question: movie.questions[currentQuestionNr].question,
+        answer: answer,
+        options: movie.questions[currentQuestionNr].options
+          ? shuffleList(
+              movie.questions[currentQuestionNr].options
+                .split(",")
+                .concat(answer)
+            )
+          : generateRandomOptions(answer),
+      });
+      setLoading(false);
+    } else {
+      history.push("/movies");
+    }
+  }, [currentQuestionNr]);
 
   const shuffleList = (list) => {
     return list.sort(() => Math.random() - 0.5);
@@ -53,22 +57,22 @@ function Game({trackData, trackTitles, database}) {
     return shuffleList(randomOptions);
   };
 
-    // Loads next question/ finishes quiz
-    const nextQuestion = (answer) => {
-        if (answer === currentQuestion.answer) {
-            setScore(score + 10);
-        }
-        if (currentQuestionNr < movie.questions.length - 1) {
-            setCurrentQuestionNr(currentQuestionNr + 1)
-        } else {
-            database.ref(`highscore/${movie.id}`).push().set({
-                'movieid': movie.id,
-                'score': score,
-                'user': user,
-            });
-            setFinished(true)
-        }
-    };
+  // Loads next question/ finishes quiz
+  const nextQuestion = (answer) => {
+    if (answer === currentQuestion.answer) {
+      setScore(score + 10);
+    }
+    if (currentQuestionNr < movie.questions.length - 1) {
+      setCurrentQuestionNr(currentQuestionNr + 1);
+    } else {
+      database.ref(`highscore/${movie.id}`).push().set({
+        movieid: movie.id,
+        score: score,
+        user: user,
+      });
+      setFinished(true);
+    }
+  };
 
   if (!loading) {
     if (!finished) {
@@ -82,7 +86,7 @@ function Game({trackData, trackTitles, database}) {
         />
       );
     } else {
-      return <GameEndView score={score} />;
+      return <GameEndView score={score} movie={movie} />;
     }
   } else return <LoadingView />;
 }
--- src/view/gameEndView.jsx
+++ src/view/gameEndView.jsx
@@ -2,20 +2,15 @@ import React from "react";
 import { Link } from "react-router-dom";
 import { Button } from "semantic-ui-react";
 
-const GameEndView = ({ score }) => {
+const GameEndView = ({ score, movie }) => {
   return (
     <div className="gameWrapper">
       <span className="gameHeader">{"Congratulations!"}</span>
       <span className="gameContent">{"Your have finished the quiz."}</span>
       <span className="gameContent">{"Your score on the quiz:"}</span>
       <span className="gameEndScore">{score}</span>
-      <Link to="/highscores/lionking">
-        <Button
-          className="gameEndButton"
-          onClick={() => console.log("End game upload highscore!")}
-        >
-          Submit your score
-        </Button>
+      <Link to={"/highscores/" + movie.urlid}>
+        <Button className="gameEndButton">See highscore</Button>
       </Link>
     </div>
   );
